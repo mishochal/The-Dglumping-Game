c14cb9046898b0b1c8bacda9bc7c5f93636e4c85
Misho Chaladze <mishochaladze4@gmail.com>
2026-04-02 15:35:09 +0400

    add simple routing for three main pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { RulesComponent } from './rules/rules.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'rules', component: RulesComponent }
+];
