c82f3418fba04016606883ed1aa4cd2dbc8ebd4f
Misho Chaladze <mishochaladze4@gmail.com>
2026-04-09 18:45:21 +0400

    add error handling

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { SignIn } from '../auth.model';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './../auth.component.css'
 })
 export class SignInComponent {
+  email: string = ""
 
+  signInData: SignIn = {
+    email: "",
+    password: ""
+  };
+
+  handleSignIn(formData: NgForm) {
+    if (formData.valid) {
+      console.log(formData)
+    }
+  }
 }
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { SignUp } from '../auth.model';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './../auth.component.css'
 })
 export class SignUpComponent {
 
+  signUpData: SignUp = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  }
+
+  handleSignUp(formData: NgForm) {
+    console.log(formData);
+  }
 }
