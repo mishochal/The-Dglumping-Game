45362797e97fcf5008dc191df425d62c9508edd4
Misho Chaladze <mishochaladze4@gmail.com>
2026-04-15 18:51:02 +0400

    add functionality to update overall and daily leaderboard after user presses DGLUMP button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed } from '@angular/core';
 import { SupabaseService } from '../supabase.service';
 import { RouterLink } from "@angular/router";
+import { DailyPlayerData } from '../leaderboard/player-data.model';
 
 @Component({
   selector: 'app-home',
@@ -13,4 +14,53 @@ export class HomeComponent {
   currentUser = computed(() => this.supabaseService.user())
 
   constructor(private supabaseService: SupabaseService) { }
+
+  async dglump() {
+    const { data, error } = await this.supabaseService.getCurrDayLeaderboard();
+    if (error) {
+      alert(error.message);
+    } else if (data) {
+      const position = await this.getPlayerPosition(data);
+      if (position > 0) {
+        alert("your position is: " + position);
+      }
+    }
+  }
+
+  async getPlayerPosition(playersData: DailyPlayerData[]): Promise<number> {
+    let position = 1;
+    if (playersData.length > 0) {
+      const nowUTC = await this.supabaseService.getUTC();
+      const lastPositionDate = playersData[playersData.length - 1].created_at;
+      const nowDate = new Date(nowUTC);
+      const lastDate = new Date(lastPositionDate);
+
+      if (this.supabaseService.isSameDate(nowDate, lastDate)) {
+        position = playersData.length + 1;
+      } else {
+        const { data, error } = await this.supabaseService.deleteCurrDayLeaderboard();
+        if (error) {
+          alert(error.message)
+          return -1;
+        }
+      }
+    }
+
+    const { data, error } = await this.supabaseService.addCurrDayLeaderboard(this.currentUser()!, position);
+    if (error) {
+      alert(error.message);
+      return -1;
+    }
+
+    const result = await this.supabaseService.updateLeaderboard(this.currentUser()!, position);
+    if (result?.error) {
+      alert(result.error.message)
+      return -1;
+    } else if (!result) {
+      alert("Could not fetch your data from the server")
+      return -1;
+    }
+
+    return position;
+  }
 }
--- src/app/supabase.service.ts
+++ src/app/supabase.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
 import { Observable } from 'rxjs';
-import { DailyPlayerData } from './leaderboard/player-data.model';
+import { DailyPlayerData, PlayerData } from './leaderboard/player-data.model';
 
 @Injectable({
   providedIn: 'root'
@@ -53,7 +53,17 @@ export class SupabaseService {
   }
 
   async updateLeaderboard(user: User, position: number) {
+    const newPlayerData = await this.getNewPlayerData(user, position);
 
+    if (newPlayerData) {
+      const { data, error } = await this.supabase.
+        from("leaderboard").
+        update(newPlayerData).
+        eq("user_id", user.id);
+
+      return { data, error };
+    }
+    return null
   }
 
   async getCurrDayLeaderboard() {
@@ -110,8 +120,10 @@ export class SupabaseService {
 
   getTableUpdates(tableName: string): Observable<any> {
     return new Observable((observer) => {
+      const uniqueChannelName = `${tableName}-${Math.random().toString(36).substring(2, 9)}`;
+
       const channel = this.supabase
-        .channel('schema-db-changes')
+        .channel(uniqueChannelName)
         .on(
           'postgres_changes',
           { event: '*', schema: 'public', table: tableName },
@@ -150,5 +162,59 @@ export class SupabaseService {
       return false;
     }
     return true;
+  }
+
+  private getPoints(position: number): number {
+    let points = 0;
+
+    switch (position) {
+      case 1:
+        points = 5;
+        break;
+      case 2:
+        points = 3;
+        break;
+      case 3:
+        points = 1;
+        break;
+      default:
+        points = 0;
+    }
+
+    return points;
+  }
+
+  private async getNewPlayerData(user: User, position: number) {
+    const { data } = await this.supabase.
+      from("leaderboard").
+      select("*").
+      eq("user_id", user.id).
+      single() as { data: PlayerData | null };
+
+    if (data) {
+      const pointsToGet = this.getPoints(position);
+
+      const newData: PlayerData = { ...data };
+      newData.points = data.points + pointsToGet;
+      newData.total_dglumps = data.total_dglumps + 1;
+
+      switch (position) {
+        case 1:
+          newData.first = data.first + 1;
+          break;
+        case 2:
+          newData.second = data.second + 1;
+          break;
+        case 3:
+          newData.third = data.third + 1;
+          break;
+        default:
+          break;
+      }
+
+      return newData;
+    } else {
+      return null;
+    }
   }
 }
