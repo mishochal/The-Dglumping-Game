d92b69a64092f8bde32f65a778883cf139a641ac
Misho Chaladze <mishochaladze4@gmail.com>
2026-04-16 14:12:14 +0400

    create daily leaderboard component

---
+++ src/app/daily-leaderboard/daily-leaderboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-leaderboard',
+  imports: [],
+  templateUrl: './daily-leaderboard.component.html',
+  styleUrl: './daily-leaderboard.component.css'
+})
+export class DailyLeaderboardComponent {
+
+}
