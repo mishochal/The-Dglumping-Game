51a2c73ad10571f2b7c1ffc0e16ed44cbbc56928
Misho Chaladze <mishochaladze4@gmail.com>
2026-04-16 16:22:03 +0400

    make dynamic daily leaderboard

--- src/app/daily-leaderboard/daily-leaderboard.component.ts
+++ src/app/daily-leaderboard/daily-leaderboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { SupabaseService } from '../supabase.service';
+import { DailyPlayerData } from '../leaderboard/player-data.model';
 
 @Component({
   selector: 'app-daily-leaderboard',
@@ -6,6 +8,38 @@ import { Component } from '@angular/core';
   templateUrl: './daily-leaderboard.component.html',
   styleUrl: './daily-leaderboard.component.css'
 })
-export class DailyLeaderboardComponent {
+export class DailyLeaderboardComponent implements OnInit {
+  medals = ["🥇", "🥈", "🥉"];
+  dailyLeaderboard = signal<DailyPlayerData[] | null>([]);
+  currentUser = computed(() => this.supabaseService.user());
 
+  constructor(private supabaseService: SupabaseService) { }
+
+  ngOnInit(): void {
+    this.getLeaderboard()
+  }
+
+  async getLeaderboard() {
+    const { data, error } = await this.supabaseService.getCurrDayLeaderboard();
+
+    if (error) {
+      alert(error.message)
+    }
+
+    this.dailyLeaderboard.set(data);
+  }
+
+  getMedal(position: number) {
+    if (position >= this.medals.length) {
+      return position + 1;
+    }
+    return this.medals[position]
+  }
+
+  isCurrentUser(user: string) {
+    if (!this.currentUser()) {
+      return false;
+    }
+    return this.currentUser()?.user_metadata?.["display_name"] === user;
+  }
 }
