ba21afe649b2a1f64034bbbf7b16a7f425a0abfd
Misho Chaladze <mishochaladze4@gmail.com>
2026-04-19 15:28:14 +0400

    update design

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,26 @@
 import { Component, computed } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SupabaseService } from '../supabase.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   currentUser = computed(() => this.supabaseService.user());
 
+  isHamburgerOpen = false;
+
   constructor(private supabaseService: SupabaseService) { }
 
   signOut() {
     this.supabaseService.signOut();
+  }
+
+  close() {
+    this.isHamburgerOpen = false;
   }
 }
