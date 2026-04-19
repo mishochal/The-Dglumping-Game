9ea80512a1e05c5968c6a2ccfa125ec32b62edc4
Misho Chaladze <mishochaladze4@gmail.com>
2026-04-19 15:54:34 +0400

    move mute condition to local storage

--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rules',
@@ -6,11 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './rules.component.html',
   styleUrl: './rules.component.css'
 })
-export class RulesComponent {
+export class RulesComponent implements OnInit {
   isMuted = false;
 
   toggleSound(audio: HTMLAudioElement) {
     this.isMuted = !this.isMuted;
+    localStorage.setItem("is_muted", JSON.stringify(this.isMuted))
     audio.currentTime = 0;
+  }
+
+  ngOnInit(): void {
+    const isMutedLocal = localStorage.getItem("is_muted");
+    if (isMutedLocal) {
+      this.isMuted = JSON.parse(isMutedLocal);
+    }
   }
 }
