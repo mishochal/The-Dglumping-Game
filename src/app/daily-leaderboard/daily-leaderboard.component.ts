2c1ce1fb59b36f55af5be565e28824e4320a1a06
Misho Chaladze <mishochaladze4@gmail.com>
2026-04-23 18:16:39 +0400

    add leaderboard for current day points

--- src/app/daily-leaderboard/daily-leaderboard.component.ts
+++ src/app/daily-leaderboard/daily-leaderboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, Signal, signal } from '@angular/core';
 import { SupabaseService } from '../supabase.service';
 import { DailyPlayerData } from '../leaderboard/player-data.model';
 import { LoaderComponent } from '../loader/loader.component';
@@ -13,27 +13,41 @@ export class DailyLeaderboardComponent implements OnInit {
   medals = ["🥇", "🥈", "🥉"];
 
   dailyLeaderboard = computed(() => this.supabaseService.dailyLeaderboard());
+  leaderboard = computed(() => this.supabaseService.leaderboard());
   currentUser = computed(() => this.supabaseService.user());
 
-  isLoading = signal<boolean>(false);
+  isDailyLoading = signal<boolean>(false);
   isNewDay = signal<boolean>(false);
+  isLeaderboardLoading = signal<boolean>(false);
+
+  isLoading = computed(() => this.isDailyLoading() || this.isLeaderboardLoading());
+  userPoints: Signal<{ username: string, points: number }[]> = computed(() => this.getUserPoints())
 
   supabaseService = inject(SupabaseService);
 
   async ngOnInit() {
+    this.isLeaderboardLoading.set(true);
     if (!this.supabaseService.isDailyLeaderboardLoaded()) {
-      await this.getLeaderboard()
+      await this.getDailyLeaderboard()
       this.supabaseService.getTableUpdates('current_day').subscribe((payload) => {
         this.updateLeaderboard(payload);
       });
     }
+    this.isLeaderboardLoading.set(false);
+
+    this.isDailyLoading.set(true);
+    if (!this.supabaseService.isLeaderboardLoaded()) {
+      await this.getLeaderboard();
+    }
+    this.isDailyLoading.set(false);
   }
 
   async getLeaderboard() {
-    this.isLoading.set(true);
-    const { data, error } = await this.supabaseService.getCurrDayLeaderboard();
-    this.isLoading.set(false);
+    const { data, error } = await this.supabaseService.getLeaderboard();
+  }
 
+  async getDailyLeaderboard() {
+    const { data, error } = await this.supabaseService.getCurrDayLeaderboard();
     if (error) {
       alert(error.message)
     }
@@ -44,7 +58,6 @@ export class DailyLeaderboardComponent implements OnInit {
       const lastDate = new Date(data[data.length - 1].created_at);
 
       if (!this.supabaseService.isSameDate(nowDate, lastDate)) {
-        console.log("vas")
         this.isNewDay.set(true);
       }
     }
@@ -89,11 +102,55 @@ export class DailyLeaderboardComponent implements OnInit {
     this.supabaseService.changeDailyLeaderboardData(newData);
   }
 
-  getTime(date: string) {
+  getTime(date: string): string {
     const dglumpDate = new Date(date);
     const hours = dglumpDate.getHours() < 10 ? "0" + dglumpDate.getHours() : dglumpDate.getHours();
     const minutes = dglumpDate.getMinutes() < 10 ? "0" + dglumpDate.getMinutes() : dglumpDate.getMinutes();
     const time = `${hours}:${minutes}`;
     return time;
+  }
+
+  getUserPoints() {
+    let usersData: { username: string, points: number }[] = [];
+
+    this.leaderboard().forEach((data) => {
+      const userId = data.user_id;
+
+      usersData.push({ username: data.username, points: this.getPoints(userId) })
+    })
+
+    const dailyLeaderboard = this.dailyLeaderboard();
+    console.log(dailyLeaderboard)
+
+    return usersData.sort((a, b) => b.points - a.points);
+  }
+
+  getPoints(userId: string): number {
+    if (this.isNewDay()) {
+      return 0;
+    }
+
+    let points = 0;
+    this.dailyLeaderboard().forEach((userData) => {
+      if (userData.user_id === userId) {
+        points += this.calcPoints(userData.position);
+      }
+    })
+
+
+    return points
+  }
+
+  private calcPoints(position: number): number {
+    switch (position) {
+      case 1:
+        return 5;
+      case 2:
+        return 3;
+      case 3:
+        return 1;
+      default:
+        return 0;
+    }
   }
 }
